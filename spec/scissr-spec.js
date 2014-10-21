c8369b11ac958463afbac2b16ddec4dd7434b2d2
rvdvyver <rvdvyver@gmail.com>
2014-10-21 13:52:28 +0200

    Remove nodes_modules folder

---
+++ spec/scissr-spec.js
@@ -0,0 +1,24 @@
+describe("Scissr.js Testing", function() {
+  // Variable should be listed below
+
+  beforeEach(function() {
+    // Run before each test.
+    // Remove if not in use.
+  });
+
+  it("Alway pass test", function() {
+    expect("1").toEqual("1");
+  });
+
+  describe("Token functionality test.", function() {
+    beforeEach(function() {
+      // Remove if not in use.
+    });
+
+    it("Token should be removed for input string", function() {
+        var output = removeToken("a,b,c","a");
+        expect(output).toBe(",b,c");
+    });
+  });
+
+});
